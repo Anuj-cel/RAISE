8db0cdca1d1a91fa7e784f6c8d740119a79c7ee9
Anuj <ajkumarmahto009@gmail.com>
2025-11-16 00:57:43 +0530

    enum added

--- Server/src/models/studentModel.js
+++ Server/src/models/studentModel.js
@@ -6,12 +6,12 @@ const studentSchema = new mongoose.Schema({
   role: { type: String, default: "student" },
   name: { type: String, required: true },
   registrationId: { type: String, required: true, unique: true, lowercase:true, trim:true }, // Registration ID
-  course: { type: String, required: true }, // B.Tech, M.Tech, MCA, PhD, etc.
+  course: { type: String,enum:["B.Tech","MCA","M.Tech","PhD"], required: true }, // B.Tech, M.Tech, MCA, PhD, etc.
   yearOfStudy: { type: Number, required:true }, // 1–4
   personalEmail: { type: String, required: true, unique: true, lowercase: true, trim: true },
   phoneNumber: { type: String },
   password:{type:String,required:true},
-  hostelName: { type: String, required: true },
+  hostelName: { type: String, enum:["A","B","C","D","E","F","G","H","I","J","K","L"],required: true },
   roomNumber: { type: String, required: true },
 }, { timestamps: true });
 
